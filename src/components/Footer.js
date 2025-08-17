0b72200e14d0226b301dda92df24b472fa8c2d19
sachin.maurya <sachin.maurya@kreatetechnologies.com>
2025-08-17 19:49:43 +0530

    Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,58 @@
-// src/components/Footer.js
+// components/Footer.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/Logo.svg';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="container">
                 <div className="footer-content">
+                    {/* Logo Section */}
                     <div className="footer-section">
-                        <img src={logo} alt="Little Lemon" className="footer-logo" />
+                        <img
+                            src="/assets/images/Logo.svg"
+                            alt="Little Lemon Logo"
+                            className="footer-logo"
+                        />
                     </div>
 
+                    {/* Doormat Navigation */}
                     <div className="footer-section">
-                        <h4>Navigation</h4>
+                        <h4>Doormat Navigation</h4>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-                            <li><Link to="#about">About</Link></li>
-                            <li><Link to="#menu">Menu</Link></li>
-                            <li><Link to="/booking">Reservations</Link></li>
-                            <li><Link to="#order">Order Online</Link></li>
-                            <li><Link to="#login">Login</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/menu">Menu</Link></li>
+                            <li><Link to="/reservations">Reservations</Link></li>
+                            <li><Link to="/order-online">Order Online</Link></li>
+                            <li><Link to="/login">Login</Link></li>
                         </ul>
                     </div>
 
+                    {/* Contact */}
                     <div className="footer-section">
                         <h4>Contact</h4>
                         <ul>
-                            <li>123 Mediterranean Ave</li>
-                            <li>Chicago, IL 60601</li>
-                            <li>(312) 555-0123</li>
-                            <li>info@littlelemon.com</li>
+                            <li>Address: 123 Main St, Chicago, IL</li>
+                            <li>Phone: (555) 123-4567</li>
+                            <li>Email: info@littlelemon.com</li>
                         </ul>
                     </div>
 
+                    {/* Social Media Links */}
                     <div className="footer-section">
-                        <h4>Social Media</h4>
+                        <h4>Social Media Links</h4>
                         <ul>
-                            <li><a href="#" aria-label="Facebook">Facebook</a></li>
-                            <li><a href="#" aria-label="Instagram">Instagram</a></li>
-                            <li><a href="#" aria-label="Twitter">Twitter</a></li>
+                            <li><a href="#" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                            <li><a href="#" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                            <li><a href="#" target="_blank" rel="noopener noreferrer">Twitter</a></li>
                         </ul>
                     </div>
                 </div>
 
                 <div className="footer-bottom">
-                    <p>&copy; 2024 Little Lemon. All rights reserved.</p>
+                    <p>&copy; 2024 Little Lemon Restaurant. All rights reserved.</p>
                 </div>
             </div>
         </footer>
