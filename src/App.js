1da9c0472fa4d409fba2bc877ac242c9e6620008
sachin.maurya <sachin.maurya@kreatetechnologies.com>
2025-08-17 18:50:48 +0530

    Set up basic routing structure with React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/global.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import BookingPage from './components/BookingPage';
+import ConfirmationPage from './components/ConfirmationPage';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/booking" element={<BookingPage />} />
+            <Route path="/confirmation" element={<ConfirmationPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
