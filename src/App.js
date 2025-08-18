64c4e98606e26b7947dbe4799e5cb972937d2a8c
sachin.maurya <sachin.maurya@kreatetechnologies.com>
2025-08-18 21:45:58 +0530

    Revert

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-
 import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/Main';
+import Homepage from './components/Homepage';
+import BookingPage from './components/BookingPage';
 import Footer from './components/Footer';
-
-// Import pages
-import About from './pages/About';
-import Menu from './pages/Menu';
-import Reservations from './pages/Reservations';
-import OrderOnline from './pages/OrderOnline';
-import Login from './pages/Login';
-import Homepage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Nav />
-        <Main>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/reservations" element={<Reservations />} />
-            <Route path="/order-online" element={<OrderOnline />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Main>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/booking" element={<BookingPage />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
